61458071b00cb08bc8cf3b5c7244ce431098557d
Mishbah <mishbah@jp74.com>
2014-09-23 00:44:49 +0100

    Change the way the cookie is stored

--- responsive/middleware.py
+++ responsive/middleware.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals
+
 import datetime
+import logging
 import re
 
 
@@ -8,8 +10,30 @@ from django.utils.encoding import force_text
 
 from .conf import settings
 
+logger = logging.getLogger('responsive')
+
 
 class ResponsiveMiddleware(object):
+
+    def process_request(self, request):
+        responsive_cookie = request.COOKIES.get(settings.RESPONSIVE_COOKIE_NAME, None)
+        if responsive_cookie:
+            parts = responsive_cookie.split(':')
+            if len(parts) != 3:
+                logger.error(
+                    'InvalidCookie: Something went wrong while decoding responsive cookie.')
+                return
+
+            try:
+                width, height, pixelratio = parts
+                width, height, pixelratio = int(width), int(height), float(pixelratio)
+            except ValueError:
+                logger.error(
+                    'ValueError: Something went wrong while decoding responsive cookie.')
+                return
+
+            print width, height, pixelratio
+
     def process_response(self, request, response):
         html_types = ('text/html', 'application/xhtml+xml')
         content_type = response.get('Content-Type', '').split(';')[0]
